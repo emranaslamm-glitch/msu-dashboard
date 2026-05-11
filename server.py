3f8a23c9ad33c0a497c797f7964a44a1b10bd997
Imran <mac@macs-MacBook-Air.local>
2026-05-11 16:36:10 +0500

    added insights page

--- server.py
+++ server.py
@@ -1,28 +1,27 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
 import requests
 import time
+import os
 
 app = Flask(__name__)
-CORS(app)
 
 CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTwjR6tFIV7fZJfGQII2VElCYac8v0DkdOqSxqmyNNK_MlF0yAWQHGmGSUvnfhqUqX14iGsBmAU1uhX/pub?gid=1773548568&single=true&output=csv"
 
 @app.route('/data')
 def get_data():
-    try:
-        response = requests.get(CSV_URL, params={'t': str(time.time())})
-        return jsonify({"csv": response.text})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    response = requests.get(CSV_URL, params={'t': time.time()})
+    return jsonify({"csv": response.text})
 
 @app.route('/')
 def home():
-    try:
-        with open('/Users/mac/MSU_Dashboard/dashboard.html') as f:
-            return f.read()
-    except Exception as e:
-        return str(e), 500
+    with open('dashboard.html') as f:
+        return f.read()
+
+@app.route('/insights')
+def insights():
+    with open('insights.html') as f:
+        return f.read()
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
